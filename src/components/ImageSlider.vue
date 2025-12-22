27f7c4b6aa553999ee6063a124196cf9bbcd06bc
Danny <dannyjag@gmail.com>
2025-12-22 19:11:15 +0000

    Installed vue3-carousel and implement the carousel slider. Only needs cleaner buttons right now

---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,35 @@
+<template>
+  <Carousel v-bind="carouselConfig">
+    <Slide v-for="(image, index) in images" :key="index">
+      <picture class="h-full w-full">
+        <source :srcset="image.desktop" media="(min-width: 768px)" />
+        <img :src="image.mobile" :alt="`Image ${index + 1}`" class="h-full w-full object-cover" />
+      </picture>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
+</template>
+
+<script setup lang="ts">
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
+import 'vue3-carousel/carousel.css';
+
+defineProps<{
+  images: {
+    mobile: string;
+    desktop: string;
+  }[];
+}>();
+
+const carouselConfig = {
+  itemsToShow: 1,
+  wrapAround: true,
+  //   autoplay: 1000,
+  pauseAutoplayOnHover: true,
+  transition: 800,
+};
+</script>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="container">main section</div>
+  <ImageSlider :images="images" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ImageSlider from './ImageSlider.vue';
+import MobileHeroImg1 from '@/assets/images/mobile-image-hero-1.jpg';
+import MobileHeroImg2 from '@/assets/images/mobile-image-hero-2.jpg';
+import MobileHeroImg3 from '@/assets/images/mobile-image-hero-3.jpg';
+import DesktopHeroImg1 from '@/assets/images/desktop-image-hero-1.jpg';
+import DesktopHeroImg2 from '@/assets/images/desktop-image-hero-2.jpg';
+import DesktopHeroImg3 from '@/assets/images/desktop-image-hero-3.jpg';
+
+const images = [
+  { mobile: MobileHeroImg1, desktop: DesktopHeroImg1 },
+  { mobile: MobileHeroImg2, desktop: DesktopHeroImg2 },
+  { mobile: MobileHeroImg3, desktop: DesktopHeroImg3 },
+];
+
+console.log(images);
+</script>
