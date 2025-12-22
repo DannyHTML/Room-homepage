d7d29795698c3d24461ba1acf1b7ce20d5b694d4
Danny <dannyjag@gmail.com>
2025-12-22 18:21:32 +0000

    Making sure when you click outside the menu, it also closes

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -7,25 +7,27 @@
     leave-from-class="opacity-100 transform translate-y-0"
     leave-to-class="opacity-0 transform -translate-y-4"
   >
-    <template v-if="open">
-      <div
-        role="menu"
-        aria-label="Mobile navigation"
-        class="fixed top-0 right-0 h-26 w-full bg-white md:hidden"
-      >
-        <div class="container flex h-full items-center justify-between">
-          <ul class="">
-            <a href="#" class="flex gap-4"
-              ><li v-for="item in menuItems" class="font-bold" :key="item">{{ item }}</li></a
-            >
-          </ul>
-          <button aria-label="Close mobile menu" @click="$emit('close')">
-            <img :src="closeIcon" alt="Close icon" />
-          </button>
-        </div>
-        <div class="bg-grey-800/70 absolute top-26 right-0 h-dvh w-full"></div>
+    <div
+      v-if="open"
+      role="menu"
+      aria-label="Mobile navigation"
+      class="fixed top-0 right-0 h-26 w-full bg-white md:hidden"
+    >
+      <div class="container flex h-full items-center justify-between">
+        <ul>
+          <a href="#" class="flex gap-4"
+            ><li v-for="item in menuItems" class="font-bold" :key="item">{{ item }}</li></a
+          >
+        </ul>
+        <button aria-label="Close mobile menu" @click="$emit('close')">
+          <img :src="closeIcon" alt="Close icon" />
+        </button>
       </div>
-    </template>
+      <div
+        @click="$emit('close')"
+        class="bg-grey-800/70 absolute top-26 right-0 h-dvh w-full"
+      ></div>
+    </div>
   </transition>
 </template>
 
