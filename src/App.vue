86932a4a18c8869568efab7c48fac384e9237719
Danny <dannyjag@gmail.com>
2025-12-24 19:13:29 +0000

    After reviewing, added images for the image sider into the content.json

--- src/App.vue
+++ src/App.vue
@@ -11,28 +11,14 @@
   </main>
 </template>
 
-<!-- TODO: Add imageSlider, add footer section -->
-
 <script setup lang="ts">
 import HeaderSite from '@/components/HeaderSite.vue';
 import ImageSlider from '@/components/ImageSlider.vue';
 import MainSection from '@/components/MainSection.vue';
-import MobileHeroImg1 from '@/assets/images/mobile-image-hero-1.jpg';
-import MobileHeroImg2 from '@/assets/images/mobile-image-hero-2.jpg';
-import MobileHeroImg3 from '@/assets/images/mobile-image-hero-3.jpg';
-import DesktopHeroImg1 from '@/assets/images/desktop-image-hero-1.jpg';
-import DesktopHeroImg2 from '@/assets/images/desktop-image-hero-2.jpg';
-import DesktopHeroImg3 from '@/assets/images/desktop-image-hero-3.jpg';
 import data from '@/assets/data/content.json';
 
 const title = data.main.hero.title;
 const description = data.main.hero.description;
 const buttonTitle = data.main.hero.buttonText;
-
-// TODO: Add imageSlider, add footer section
-const images = [
-  { mobile: MobileHeroImg1, desktop: DesktopHeroImg1 },
-  { mobile: MobileHeroImg2, desktop: DesktopHeroImg2 },
-  { mobile: MobileHeroImg3, desktop: DesktopHeroImg3 },
-];
+const images = data.imageSlider.images;
 </script>
